449c2b1f0ad3c685fd876cd96eecbd04186ddd8f
zhiyong4113 <zhiyong.ong@hotmail.com>
2016-09-14 01:23:36 +0800

    Add throw exception to Command class execute method.

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -38,8 +38,9 @@ public abstract class Command {
 
     /**
      * Executes the command and returns the result.
+     * @throws Exception 
      */
-    public abstract CommandResult execute();
+    public abstract CommandResult execute() throws Exception;
 
     /**
      * Supplies the data the command will operate on.
